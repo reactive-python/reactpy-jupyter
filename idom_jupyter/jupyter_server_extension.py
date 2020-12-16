1e00fdfe6b1c4898ff2442103dc4d66309d1abdf
rmorshea <ryan.morshead@gmail.com>
2020-12-15 22:22:02 -0800

    find current notebook base URL

--- idom_jupyter/jupyter_server_extension.py
+++ idom_jupyter/jupyter_server_extension.py
@@ -1,11 +1,12 @@
 from urllib.parse import urljoin
 
+from notebook.notebookapp import NotebookApp
 from idom.client.manage import BUILD_DIR
 from tornado.web import StaticFileHandler
 from tornado.web import Application
 
 
-def load_jupyter_server_extension(notebook_app):
+def load_jupyter_server_extension(notebook_app: NotebookApp):
     web_app: Application = notebook_app.web_app
     route_pattern = urljoin(web_app.settings["base_url"], "_idom_web_modules/(.*)")
     web_app.add_handlers(
