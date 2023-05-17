946d4308773f9fd0e3d8266cd16cb203c8819596
rmorshea <ryan.morshead@gmail.com>
2023-05-17 10:56:36 -0600

    allow jupyter widgets to be used in reactpy components

--- reactpy_jupyter/__init__.py
+++ reactpy_jupyter/__init__.py
@@ -5,21 +5,22 @@
 # Distributed under the terms of the Modified BSD License.
 
 from . import jupyter_server_extension
+from .widget_component import from_widget
 from .import_resources import setup_import_resources
 from .ipython_extension import load_ipython_extension, unload_ipython_extension
-from .widget import LayoutWidget, run, set_import_source_base_url, widgetize
+from .layout_widget import to_widget, run, set_import_source_base_url
 
 __version__ = "0.8.1"  # DO NOT MODIFY
 
-__all__ = [
-    "LayoutWidget",
-    "widgetize",
-    "run",
+__all__ = (
+    "from_widget",
     "load_ipython_extension",
     "unload_ipython_extension",
+    "to_widget",
+    "run",
     "set_import_source_base_url",
     "jupyter_server_extension",
-]
+)
 
 
 setup_import_resources()
---
+++ reactpy_jupyter/widget_component.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import Callable
+from attr import dataclass
+from ipywidgets import Widget
+
+from reactpy import component, create_context, use_context, use_effect, html
+from reactpy.types import VdomDict, Context
+
+
+inner_widgets_context: Context[InnerWidgets | None] = create_context(None)
+
+
+@component
+def from_widget(source: Widget) -> VdomDict:
+    inner_widgets = use_context(inner_widgets_context)
+
+    @use_effect
+    def add_widget():
+        inner_widgets.add(source)
+        return lambda: inner_widgets.remove(source)
+
+    if inner_widgets is None:
+        raise RuntimeError("Jupyter component must be rendered inside a JupyterLayout")
+
+    return html.span({"class": f"widget-model-id-{source.model_id}"})
+
+
+@dataclass
+class InnerWidgets:
+    add: Callable[[Widget], None]
+    remove: Callable[[Widget], None]
