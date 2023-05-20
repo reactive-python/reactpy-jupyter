9031259ce50021e78cf41f077598e7893f3bd568
rmorshea <ryan.morshead@gmail.com>
2023-05-20 00:43:40 -0600

    add a use_trait hook attached to widget wrapper

--- reactpy_jupyter/__init__.py
+++ reactpy_jupyter/__init__.py
@@ -5,8 +5,9 @@
 # Distributed under the terms of the Modified BSD License.
 
 from . import jupyter_server_extension
+from .component_widget import run, set_import_source_base_url, to_widget
+from .hooks import use_trait
 from .import_resources import setup_import_resources
-from .layout_widget import run, set_import_source_base_url, to_widget
 from .monkey_patch import execute_patch
 from .widget_component import from_widget
 
@@ -14,12 +15,13 @@ __version__ = "0.9.5"  # DO NOT MODIFY
 
 __all__ = (
     "from_widget",
+    "jupyter_server_extension",
     "load_ipython_extension",
-    "unload_ipython_extension",
-    "to_widget",
     "run",
     "set_import_source_base_url",
-    "jupyter_server_extension",
+    "to_widget",
+    "unload_ipython_extension",
+    "use_trait",
 )
 
 
